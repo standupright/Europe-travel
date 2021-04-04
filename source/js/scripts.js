2efab94e210d6cf6621449362ad8124c51af26be
standupright <standupright@mail.ru>
2021-04-04 17:43:32 +0500

    Сверстан section country-description

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -13,3 +13,22 @@ navToggle.addEventListener('click', function() {
     nav.classList.remove('nav--opened');
   }
 });
+
+// switch tabs
+const tabs = document.querySelectorAll('.tabs-header__tab');
+const slides = document.querySelectorAll('.country-description__country-item');
+
+const addSliderClickHandler = function (button,slide) {
+    button.addEventListener("click", function(){
+        for (var i=0; i < tabs.length; i++){
+          tabs[i].classList.remove("tabs-header__tab--active");
+          slides[i].classList.remove("country-description__country-item--active");
+        }
+        slide.classList.add("country-description__country-item--active");
+        button.classList.add("tabs-header__tab--active");
+    });
+};
+
+for (var i = 0; i < tabs.length; i++){
+   addSliderClickHandler(tabs[i],slides[i]);
+}
